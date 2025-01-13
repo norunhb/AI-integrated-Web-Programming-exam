920233170f0cee8a5b65cf53bede7109c3a9d5f5
norunhb <norunhb@gmail.com>
2025-01-13 17:10:01 +0100

    commit files

---
+++ Eksamen/script.js
@@ -0,0 +1,26 @@
+const hamMenu = document.querySelector('.ham-menu');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click',()=>{
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
+
+
+const form= document.getElementById('contactform')
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event){
+    event.preventDefault();
+    const name=document.getElementById('name').value;
+    const email=document.getElementById('email').value;
+    
+
+    const inputs= document.querySelectorAll'name', 'email');
+
+    inputs.array.forEach(input => {
+        input.value=''
+    });
+}
+
